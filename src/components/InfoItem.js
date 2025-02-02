801b20d7a05785c08c059ee248b7221d825ea3e6
Ali Mirlou <alimirlou@gmail.com>
2025-02-02 19:59:54 +0330

    Add className prop to some components

--- src/components/InfoItem.js
+++ src/components/InfoItem.js
@@ -1,8 +1,8 @@
-export default function InfoItem({ label, children }) {
+export default function InfoItem({ label, className = "", children }) {
   return (
-    <div className="grid grid-cols-3">
-      <div className="font-medium text-sm lg:text-base">{label}</div>
-      <div className="text-gray-600 dark:text-gray-400 text-sm lg:text-base col-span-2">
+    <div className={`grid grid-cols-7 ${className}`}>
+      <div className="font-medium text-sm lg:text-base col-span-2">{label}</div>
+      <div className="text-gray-600 dark:text-gray-400 text-sm lg:text-base col-span-5">
         {children}
       </div>
     </div>
--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function LinkItem({ href, icon: Icon, children }) {
+export default function LinkItem({ href, icon: Icon, className = "", children }) {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-blue-600 dark:text-blue-400 hover:underline flex items-center gap-2 min-w-0 mb-2 text-sm md:text-base"
+      className={`text-blue-600 dark:text-blue-400 hover:underline flex items-center gap-2 min-w-0 mb-2 text-sm md:text-base ${className}`}
     >
       {/* If Icon is an element (like img), render it directly */}
       {React.isValidElement(Icon) ? Icon : Icon && <Icon className="text-xl flex-shrink-0" />}
